c2c026a1e5f30e047829d20e7eb7d4658c78e6cf
ma nan <manan@snaplingo.com>
2018-09-21 10:38:15 +0800

    add docker db file

--- compoments/db-docker-file/create_db.sh
+++ compoments/db-docker-file/create_db.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+##关闭数据库
+docker-compose -f mysql.yml down
+docker-compose -f redis.yml down
+docker-compose -f pg.yml down
+
+##打开数据库
 docker-compose -f mysql.yml up
-docker-compose -f redis.yaml up
+docker-compose -f redis.yml up
+docker-compose -f pg.yml up
 ##如果需要用navicat连接mysql,可以授权改密码什么的
