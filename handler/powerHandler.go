63bc09b427f9c4c2c7b3985fa30e2b59c3ac763e
ma nan <manan@snaplingo.com>
2018-09-25 09:24:51 +0800

    init user permission

---
+++ handler/powerHandler.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"self-game/utils/vo"
+)
+
+func GetUserPowerList(c *gin.Context) {
+	retData := vo.NewData()
+	defer SendResponse(c, retData)
+
+	var (
+		uid       string
+		poserList interface{}
+	)
+
+	logger.Infof("userPowerList:uid[%v],powerInfo=%v", uid, poserList)
+	return
+}
--- router/router.go
+++ router/router.go
@@ -10,24 +10,32 @@ import (
 
 func Router(r *gin.Engine) {
 
-	wxServerCheck:= r.Group("/api/v2")
+	wxServerCheck := r.Group("/api/v2")
 	{
 		wxServerCheck.GET("/", handler.HandlerSignatureHandler)
 
 	}
 
-	cc:=r.Group("/api/v1",handler.VerifyToken)
+	cc := r.Group("/api/v1", handler.VerifyToken)
 	userLoginGroup := cc.Group("/anonymous")
 	{
 		userLoginGroup.POST("/user/register", handler.RegisterUserHandler)
 		userLoginGroup.POST("/user/login", handler.UserLoginHandler)
 	}
 
+	// 用户相关
 	user := cc.Group("/user", handler.VerifyUserToken)
 	{
 		user.GET("/name/:uid", handler.GetUserNameHandler)
 		user.POST("/name", handler.PostUserNameHandler)
 		user.GET("/health_check", handler.ConsulHealthCheck)
 	}
+
+	// 权限相关
+	powerGroup := cc.Group("/power")
+	{
+		powerGroup.GET("/list:uid")
+	}
+
 	r.Run(fmt.Sprintf(":%d", config.Config.Cfg.Port))
 }
