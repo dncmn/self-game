a4c4c892993fa51055d6d25b855c97b19a2243ec
ma nan <manan@snaplingo.com>
2018-12-11 15:45:15 +0800

    临时操作:关闭服务器的时候，进行资源释放操作

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
 	"self-game/router"
+	"self-game/service"
 )
 
 func main() {
-	router.Router(gin.Default())
+	c := make(chan os.Signal)
+	go func() {
+		signal.Notify(c)
+		fmt.Println("begin")
+		router.Router(gin.Default())
+	}()
+	<-c                    // 当关闭服务器的时候,就进行一些释放资源的操作
+	service.IsStop = false // 信号量,表示是否跳出这个循环,然后进行一系列的资源释放操作
 }
--- service/wxServer.go
+++ service/wxServer.go
@@ -1,1 +1,27 @@
-package  service
+package service
+
+import (
+	"self-game/config"
+	"self-game/utils"
+	"self-game/utils/async"
+	"time"
+)
+
+var (
+	IsStop = true
+)
+
+func init() {
+	go async.Do(func() {
+		initService()
+	})
+}
+
+func initService() {
+	logger.Info("sync info begin")
+	for IsStop {
+		logger.Infof("current_time=%v", utils.GetTimeZoneTime(config.Config.Cfg.TimeZone).Unix())
+		time.Sleep(time.Second)
+	}
+	logger.Info("sync info end")
+}
