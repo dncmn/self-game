6e16483eeaf8951e5566af5194e7153835c942e4
manan <manan@snaplingo.com>
2019-05-22 10:27:32 +0800

    refactor dir name

--- controller/codeHandler.go
+++ controller/codeHandler.go
@@ -1,4 +1,4 @@
-package handler
+package controller
 
 import (
 	"github.com/boombuler/barcode/qr"
--- router/router.go
+++ router/router.go
@@ -5,8 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"self-game/config"
+	"self-game/controller"
 	_ "self-game/dao"
-	"self-game/handler"
 	"self-game/utils/qrcode"
 )
 
@@ -14,48 +14,48 @@ func Router(r *gin.Engine) {
 	r.StaticFS("/compoments/runtime/qrcode", http.Dir(qrcode.GetQrCodeFullPath()))
 	wxServerCheck := r.Group("/api/v2")
 	{
-		wxServerCheck.GET("/check", handler.HandlerSignatureHandler) // 在官网上配置服务器签名
-		wxServerCheck.POST("/check", handler.HandMessagesHandler)    // 被动回复消息
+		wxServerCheck.GET("/check", controller.HandlerSignatureHandler) // 在官网上配置服务器签名
+		wxServerCheck.POST("/check", controller.HandMessagesHandler)    // 被动回复消息
 
 	}
 
-	cc := r.Group("/api/v1", handler.VerifyToken)
+	cc := r.Group("/api/v1", controller.VerifyToken)
 	userLoginGroup := cc.Group("/anonymous")
 	{
-		userLoginGroup.POST("/user/register", handler.RegisterUserHandler)
-		userLoginGroup.POST("/user/login", handler.UserLoginHandler)
-		userLoginGroup.GET("/login_by_uid", handler.GetUserLoginHandler)
+		userLoginGroup.POST("/user/register", controller.RegisterUserHandler)
+		userLoginGroup.POST("/user/login", controller.UserLoginHandler)
+		userLoginGroup.GET("/login_by_uid", controller.GetUserLoginHandler)
 	}
 
 	// 用户相关
-	user := cc.Group("/user", handler.VerifyUserToken)
+	user := cc.Group("/user", controller.VerifyUserToken)
 	{
-		user.GET("/name/:uid", handler.GetUserNameHandler)
-		user.POST("/name", handler.PostUserNameHandler)
-		user.GET("/health_check", handler.ConsulHealthCheck)
+		user.GET("/name/:uid", controller.GetUserNameHandler)
+		user.POST("/name", controller.PostUserNameHandler)
+		user.GET("/health_check", controller.ConsulHealthCheck)
 	}
 
 	// 二维码相关
 	codeGroup := cc.Group("/qrcode")
 	{
-		codeGroup.GET("/", handler.GetCodeImageHandler)    // 产生二维码
-		codeGroup.GET("/info", handler.GetCodeInfoHandler) // 获取二维码信息---是一个url，就是从那里获取信息
+		codeGroup.GET("/", controller.GetCodeImageHandler)    // 产生二维码
+		codeGroup.GET("/info", controller.GetCodeInfoHandler) // 获取二维码信息---是一个url，就是从那里获取信息
 	}
 
 	// 微信相关
 	wechatGroup := cc.Group("/wechat")
 	{
-		wechatGroup.GET("/accesstokne_by_code", handler.WechatAuthHandler)                    // code 换access_token或openid
-		wechatGroup.GET("/download_media_by_audioid", handler.WechatDownloadMediaDataHandler) // 根据media_id下载音频数据
-		wechatGroup.POST("/send_template_info", handler.WechatSendTemplateInfoHandler)        // 发送模板消息
-		wechatGroup.GET("/jsconfig", handler.WechatGetJSConfigHandler)                        // 获取jsconfig
-		wechatGroup.POST("/receive_msg", handler.WechatReceiveMsgHandler)                     // 接收各种类型的消息
+		wechatGroup.GET("/accesstokne_by_code", controller.WechatAuthHandler)                    // code 换access_token或openid
+		wechatGroup.GET("/download_media_by_audioid", controller.WechatDownloadMediaDataHandler) // 根据media_id下载音频数据
+		wechatGroup.POST("/send_template_info", controller.WechatSendTemplateInfoHandler)        // 发送模板消息
+		wechatGroup.GET("/jsconfig", controller.WechatGetJSConfigHandler)                        // 获取jsconfig
+		wechatGroup.POST("/receive_msg", controller.WechatReceiveMsgHandler)                     // 接收各种类型的消息
 	}
 
 	// 权限相关
 	powerGroup := cc.Group("/power")
 	{
-		powerGroup.GET("/list:uid", handler.GetUserPowerListHandler) // 列举某个用户的权限
+		powerGroup.GET("/list:uid", controller.GetUserPowerListHandler) // 列举某个用户的权限
 	}
 
 	err := r.Run(fmt.Sprintf(":%d", config.Config.Cfg.Port))
